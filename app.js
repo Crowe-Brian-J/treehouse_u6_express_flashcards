3274b47f84f4c0b9e3b64a2fe1ea233e154e7868
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-31 22:02:12 -0400

    invoke express, set up server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+// Require express
 const express = require('express')
+
+// Invoke express
+const app = express()
+
+// Set server up - Currently sends error because we haven't set anything up to retrieve from the server
+app.listen(3000)
