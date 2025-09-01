41628b5b06354d1e92b6c77879e7a8d26dd38f9c
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-31 22:22:03 -0400

    add second route

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const express = require('express')
 // Invoke express
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send('I love Treehouse!')
+//ROUTE - How to respond on GET request
+app.get('/', (req, res) => {
+  res.send('<h1>I love Treehouse!</h1>')
+})
+
+//ROUTE - How to respond on GET request (on /hello)
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello, JavaScript Developer!</h1>')
 })
 
 // Set server up - Currently sends error because we haven't set anything up to retrieve from the server
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000') //Broadcast what's happening
+})
