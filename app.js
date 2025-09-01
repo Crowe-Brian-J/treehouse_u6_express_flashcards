190f100825da4e3bad9e59c901cec72252db9672
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-31 22:14:38 -0400

    app.get() to respond to request from client

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const express = require('express')
 // Invoke express
 const app = express()
 
+app.get('/', (request, response) => {
+  response.send('I love Treehouse!')
+})
+
 // Set server up - Currently sends error because we haven't set anything up to retrieve from the server
 app.listen(3000)
