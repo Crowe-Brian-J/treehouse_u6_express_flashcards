7f3fe817d23397dbd8f5df22e8af8612d352cc28
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-01 20:24:54 -0400

    move routes to routes/index.js

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+
+// ROUTE - GET request
+router.get('/', (req, res) => {
+  const name = req.cookies.username
+  if (typeof name === 'undefined') {
+    res.redirect('/hello')
+  } else {
+    res.render('index', { title: 'Flash Cards', name })
+  }
+})
+
+// ROUTE - GET request (on /cards)
+router.get('/cards', (req, res) => {
+  res.render('card', {
+    title: 'Flash Cards',
+    prompt: "Who is buried in Grant's tomb?",
+    hint: "Think about who's tomb it is."
+  })
+})
+
+// ROUTE - GET request (on /hello)
+router.get('/hello', (req, res) => {
+  const name = req.cookies.username
+  if (typeof name === 'undefined') {
+    res.render('hello', { name: req.cookies.username }) // Plural
+  } else {
+    res.redirect('/')
+  }
+})
+
+// ROUTE - POST request (on /logout)
+router.post('/logout', (req, res) => {
+  res.clearCookie('username')
+  res.redirect('hello')
+})
+
+// ROUTE - POST request (on /hello)
+router.post('/hello', (req, res) => {
+  res.cookie('username', req.body.userName) // Singular
+  res.redirect('/')
+})
